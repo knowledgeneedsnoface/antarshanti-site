e807dfaea70cffe728b2bcd320af8b110e513c5c
Siddharth Chouhan <sid@Siddharths-MacBook-Air.local>
2025-12-05 17:20:59 +0530

    Add animated Hero Section with SVG mandala and parallax

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 // app/page.tsx
 "use client";
 import React from "react";
+import HeroSection from "./(components)/HeroSection";
 import JourneyIntro from "./(components)/JourneyIntro";
 import BenefitsCarousel from "./(components)/BenefitsCarousel";
 import ProductCard from "./(components)/ProductCard";
@@ -8,13 +9,21 @@ import ProductCard from "./(components)/ProductCard";
 export default function Home() {
   const [started, setStarted] = React.useState(false);
 
-  return (
-    <div>
-      {!started ? <JourneyIntro onStart={() => setStarted(true)} /> : null}
-      <main>
-        <BenefitsCarousel />
-        <ProductCard />
-      </main>
-    </div>
-  );
+return (
+  <div>
+    {!started ? (
+      <JourneyIntro onStart={() => setStarted(true)} />
+    ) : (
+      <>
+        <HeroSection />
+        <main>
+          <BenefitsCarousel />
+          <ProductCard />
+        </main>
+      </>
+    )}
+  </div>
+);
+
+
 }
